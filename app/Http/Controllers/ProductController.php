eeba1b0551c0dd2cc67b30332fadff7a181f6365
karine <karine.porto@gmail.com>
2024-03-20 12:31:15 +0000

    Sanctum, breeze and vue updates

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
 use App\Models\Product;
+use Inertia\Inertia;
 
 class ProductController extends Controller
 {
@@ -13,7 +14,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        //return Inertia::render('Product/Index');
+        return inertia('Products/Index');
     }
 
     /**
