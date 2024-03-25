e9ddfa254a19850ac46a202b87284e1cbdfcbd60
karine <karine.porto@gmail.com>
2024-03-25 15:41:56 +0000

    Working with accessors and muttators

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -41,10 +41,10 @@ class StoreProductRequest extends FormRequest
         ];
     }
 
-    public function prepareForValidation(){
+  /*   public function prepareForValidation(){
         $this->merge([
             'price' => $this->price * 100
         ]);
-    }
+    } */
 
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,13 +10,24 @@ class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'brand', 'category_id','price','weight','description'];
+    protected $fillable = ['name', 'brand', 'category_id', 'price', 'weight', 'description'];
 
-    public function category(){
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
 
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class);
+    }
+
+    protected function price(): Attribute
+    {
+
+        return Attribute::make(
+            set: fn (int $value) => $value * 100,
+            get: fn (int $value) => $value/100
+        );
     }
 }
