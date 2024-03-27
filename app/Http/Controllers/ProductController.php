72d2d8c63f72780dd77ee3f9cc4affb19df110b9
karine <karine.porto@gmail.com>
2024-03-27 16:17:01 +0000

    Showing the detail of a resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -44,7 +44,9 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return inertia('Product/Show',[
+            'product' => ProductResource::make($product)
+        ]);
     }
 
     /**
