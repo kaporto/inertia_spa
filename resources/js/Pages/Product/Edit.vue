037494a7f4bebea48d6a2540ea0a9cba36528718
karine <karine.porto@gmail.com>
2024-04-01 17:01:33 +0100

    Deleting resource

--- resources/js/Pages/Product/Edit.vue
+++ resources/js/Pages/Product/Edit.vue
@@ -1,15 +1,24 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import ProductForm from "./ProductForm.vue";
-import { Head, Link, useForm } from '@inertiajs/vue3';
+import { Head, Link, useForm, router } from '@inertiajs/vue3';
+
+
+const deleteRow = (id) => {
+    if(window.confirm("Are you sure?")){
+        router.delete(route('products.destroy',id),{
+            preserveScroll:true
+        })
+    }
+}
 
 const props = defineProps({
     categories: {
         type: Array,
         required: true
     },
-    product:{
-        type:Object,
+    product: {
+        type: Object,
         required: true
     }
 })
@@ -23,7 +32,7 @@ const props = defineProps({
     description: ''
 }) */
 
-const form = useForm({...props.product, category_id: props.product.category.id})
+const form = useForm({ ...props.product, category_id: props.product.category.id })
 
 const update = () => {
     form.put(route('products.update', form.id), {
@@ -43,7 +52,7 @@ const update = () => {
 
                 <Link :href="route('products.index')"
                     class="px-3 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-md hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-                Manage Products</Link>               
+                Manage Products</Link>
             </div>
 
         </template>
@@ -55,14 +64,18 @@ const update = () => {
                         <div class="flex items-center justify-center">
                             <div class="relative w-full max-w-2xl max-h-full">
                                 <ProductForm :form="form" :categories="categories" @submit="update">
-                                    <button type="submit"
-                                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Update</button>
-                                    <Link :href="route('products.index')"
-                                        class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5">Cancel</Link>
-
-
+                                    <div class="flex items-center justify-between">
+                                        <div class="space-x-2">
+                                            <button type="submit"
+                                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Update</button>
+                                            <Link :href="route('products.index')"
+                                                class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5">
+                                            Cancel</Link>
+                                        </div>
+                                        <a href="#" class="font-medium text-red-600 hover:underline" @click.prevent="deleteRow(product.id)">Delete</a>
+                                    </div>
                                 </ProductForm>
-                               
+
                             </div>
                         </div>
                     </div>
