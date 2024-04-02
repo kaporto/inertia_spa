e24abbdf7e0d80306fc6587c55d8d1476b431503
karine <karine.porto@gmail.com>
2024-04-02 15:36:01 +0100

    Creating pagination component

--- resources/js/Pages/Product/Index.vue
+++ resources/js/Pages/Product/Index.vue
@@ -1,10 +1,11 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import Pagination from '@/Components/Pagination.vue';
 import { Head, Link, router } from '@inertiajs/vue3';
 
 defineProps({
     products: {
-        type: Array,
+        type: Object,
         required: true
     }
 })
@@ -59,7 +60,7 @@ const deleteRow = (id) => {
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr v-for="product in products" :key="product.id"
+                                <tr v-for="product in products.data" :key="product.id"
                                     class="bg-white border-b hover:bg-gray-50">
                                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                                         {{ product.name }}
@@ -81,43 +82,7 @@ const deleteRow = (id) => {
                                 </tr>
                             </tbody>
                         </table>
-                        <nav class="flex items-center flex-column flex-wrap md:flex-row justify-between py-2 px-4"
-                            aria-label="Table navigation">
-                            <span
-                                class="text-sm font-normal text-gray-700 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing
-                                <span class="font-semibold text-gray-700">1-10</span> of <span
-                                    class="font-semibold text-gray-700">1000</span></span>
-                            <ul class="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
-                                <li>
-                                    <a href="#"
-                                        class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 hover:text-gray-700">Previous</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                        class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 hover:text-gray-700">1</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                        class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 hover:text-gray-700">2</a>
-                                </li>
-                                <li>
-                                    <a href="#" aria-current="page"
-                                        class="flex items-center justify-center px-3 h-8 text-blue-600 hover:text-blue-700">3</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                        class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 hover:text-gray-700">4</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                        class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 hover:text-gray-700">5</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                        class="flex items-center justify-center px-3 h-8 leading-tight text-stone-400 hover:text-stone-400 cursor-not-allowed">Next</a>
-                                </li>
-                            </ul>
-                        </nav>
+                        <Pagination :meta="products.meta"/>
                     </div>
 
                 </div>
