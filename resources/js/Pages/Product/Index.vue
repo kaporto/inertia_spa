f66d4020afae009286c8a9f2728e14bcf92410df
karine <karine.porto@gmail.com>
2024-04-12 15:37:01 +0100

    Loading selected products and displaying categories

--- resources/js/Pages/Product/Index.vue
+++ resources/js/Pages/Product/Index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import Pagination from '@/Components/Pagination.vue';
 import Sortable from "@/Components/Sortable.vue";
@@ -16,12 +16,22 @@ const props = defineProps({
         type: Object,
         required: true
     },
+    categories: {
+        type: Array,
+        required: true
+    },
     query: {
         type: Object,
         default: () => ({
             search: ''
         })
     }
+})
+
+const  selectedProducts = computed(() => {
+    return props.products.data
+                .filter( (product) => selectedIds.value.includes(product.id))
+                .map( (product) => ({ id: product.id, name: product.name}))
 })
 
 const handleSearch = (event) => {
@@ -160,6 +170,6 @@ const deleteSelected = () => {
                 </div>
             </div>
         </div>
-        <BulkEdit :show="showModal" @close="showModal = false" />
+        <BulkEdit :show="showModal" @close="showModal = false" :products="selectedProducts" :categories="categories"/>
     </AuthenticatedLayout>
 </template>
